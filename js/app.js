4340c826617edd21990f5db6c7050ddba53116b9
Dan <dantdesign@outlook.com>
2019-03-20 07:08:01 +1100

    Changing to un-dynamic list of cards (html)

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * variables
  */
 const deck = document.querySelector(".deck");
+let openCards = [];
 
 /*
  * Create a list that holds all of your cards
@@ -31,16 +32,20 @@ const cards = [
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
 shuffle(cards);
 
 for (const card of cards) {
+
     const li = document.createElement("li");
     const icon = document.createElement("i");
+
     icon.classList.add("fa");
     icon.classList.add(card);
     li.classList.add("card");
     li.appendChild(icon);
     deck.appendChild(li);
+    
 }
 
 
@@ -49,7 +54,7 @@ for (const card of cards) {
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
 
-    if (currentIndex !== 0) {
+    while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         temporaryValue = array[currentIndex];
@@ -62,20 +67,53 @@ function shuffle(array) {
 
 /*
  * set up the event listener for a card. If a card is clicked:
+
  *  - display the card's symbol (put this functionality in another function that you call from this one)
+ * 
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * 
  *  - if the list already has another card, check to see if the two cards match
+ * 
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ * 
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ * 
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ * 
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
 deck.addEventListener("click", function(e) {
-    if (e.target.classList.contains("card")) {
-        displaySymbol(e.target);
+
+    const selection = e.target;
+
+    if (selection.classList.contains("card")) {
+        displaySymbol(selection);
+        console.log(openCards);
+        for (const card of openCards) {
+            if (card.classList.contains("open", "show")) {
+                console.log(Yes);
+            }
+        }
+    }
+
+    function displaySymbol(arg) {
+        arg.classList.add("open", "show");
+        addCard(arg);
+    };
+    
+    function addCard(arg) {
+        let icon = arg.firstElementChild;
+        openCards.push(icon);
+    };
+
+    function isMatch(arg) {
+        arg.classList.add("match");
+    }
+
+    function noMatch(arg1, arg2) {
+        if (arg1 !== arg2) {
+            return false;
+        }
     }
 });
-
-function displaySymbol(arg) {
-    arg.classList.add("match");
-};
