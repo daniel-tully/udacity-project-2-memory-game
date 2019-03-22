ef2ec1cb96e809fcacb7f3d81ed6582dcff6eba8
Dan <dantdesign@outlook.com>
2019-03-22 15:39:48 +1100

    Working on the restart initGame. Need to remove the previous games elements before injecting the new ones

--- js/app.js
+++ js/app.js
@@ -2,49 +2,50 @@
  * Create a list that holds all of your cards
  */
 var cards = [
-    "fa-diamond",
-    "fa-paper-plane-o",
-    "fa-anchor",
-    "fa-bolt",
-    "fa-cube",
-    "fa-leaf",
-    "fa-bicycle",
-    "fa-bomb",
-    "fa-diamond",
-    "fa-paper-plane-o",
-    "fa-anchor",
-    "fa-bolt",
-    "fa-cube",
-    "fa-leaf",
-    "fa-bicycle",
-    "fa-bomb"
-]
-var deck = document.querySelector(".deck");
-var moveCounterSpan = document.querySelector(".moves");
+    'fa-diamond',
+    'fa-paper-plane-o',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-cube',
+    'fa-leaf',
+    'fa-bicycle',
+    'fa-bomb',
+    'fa-diamond',
+    'fa-paper-plane-o',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-cube',
+    'fa-leaf',
+    'fa-bicycle',
+    'fa-bomb'
+];
+var deck = document.querySelector('.deck');
+var moveCounterSpan = document.querySelector('.moves');
 var moveCount = 0;
 var pairCount = 0;
 var openCards = [];
 
-// shuffle the list of cards using the provided "shuffle" method below
-shuffle(cards);
-// loop through each card and create its HTML add each card's HTML to the page
-for (const card of cards) {
+window.onload = function() {
+    initGame();
+};
 
-    const li = document.createElement("li");
-    const icon = document.createElement("i");
-    icon.classList.add("fa");
-    icon.classList.add(card);
-    li.classList.add("card");
-    li.appendChild(icon);
-    deck.appendChild(li);
-
+function restartGame() {
+    shuffle(cards);
+    deck.remove(children);
+    for (const card of cards) {
+        const li = document.createElement('li');
+        const icon = document.createElement('i');
+        icon.classList.add('fa');
+        icon.classList.add(card);
+        li.classList.add('card');
+        li.appendChild(icon);
+        deck.appendChild(li);
+    }
 }
-
-
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -57,31 +58,19 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
-deck.addEventListener("click", function(e) {
+deck.addEventListener('click', function(e) {
 
     const selection = e.target;
 
-    if (selection.classList.contains("card")) {
-
+    if (selection.classList.contains('card')) {
         displaySymbol(selection);
-
         // check if cards match
         if (openCards.length > 1) {
             moveCounter();
             if (openCards[0].value === openCards[1].value) {
                 isMatch();
-                if (pairCount === 2) {
+                if (pairCount === 1) {
                     pairCounter();
                     restartGame();
                 }
@@ -89,15 +78,14 @@ deck.addEventListener("click", function(e) {
                 noMatch();
             }
         }
-    
     }
 
     // show card function
     function displaySymbol(arg) {
-        arg.classList.add("open", "show");
+        arg.classList.add('open', 'show');
         addCard(arg);
     };
-    
+
     // add card to 'openCards' function
     function addCard(arg) {
         let icon = arg.firstElementChild.classList;
@@ -107,9 +95,9 @@ deck.addEventListener("click", function(e) {
     // cards match
     function isMatch() {
         for (const card of deck.children) {
-            if (card.classList.contains("open")) {
-                card.classList.add("match");
-                card.classList.remove("open", "show");
+            if (card.classList.contains('open')) {
+                card.classList.add('match');
+                card.classList.remove('open', 'show');
             }
         }
         openCards = [];
@@ -120,7 +108,7 @@ deck.addEventListener("click", function(e) {
     function noMatch() {
         setTimeout(function () {
             for (const card of deck.children) {
-                card.classList.remove("open", "show");
+                card.classList.remove('open', 'show');
             }
             openCards = [];
         }, 1000);
@@ -134,24 +122,27 @@ deck.addEventListener("click", function(e) {
 
     function pairCounter() {
         setTimeout(function () {
-            var successContainer = document.querySelector(".success-container");
-            var successCount = document.querySelector(".success-count");
+            var successContainer = document.querySelector('.success-container');
+            var successCount = document.querySelector('.success-count');
             successCount.innerHTML = moveCount;
-            successContainer.style.display = "flex";
+            successContainer.style.display = 'flex';
         },300);
     }
 
     function restartGame() {
-        var restartButton = document.querySelector(".restart-button");
-        restartButton.addEventListener("click", function (){
-            var successContainer = document.querySelector(".success-container");
-            successContainer.style.display = "none";
+        var restartButton = document.querySelector('.restart-button');
+        restartButton.addEventListener('click', function (){
+            var successContainer = document.querySelector('.success-container');
+            successContainer.style.display = 'none';
             // zero counter
             moveCounterSpan.innerHTML = 0;
             // clear all cards
             for (let i = 0; i < deck.children.length; i++) {
-                deck.children[i].classList.remove("match");
+                deck.children[i].classList.remove('match');
             }
+            moveCount = 0;
+            pairCount = 0;
+            initGame();
         });
     }
 
