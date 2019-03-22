bc3fabebdf05a4d7a16d7fd65c493fea9a982665
Dan <dantdesign@outlook.com>
2019-03-23 10:17:52 +1100

    if statement now controlling the amount clicks per go to 2

--- js/app.js
+++ js/app.js
@@ -17,6 +17,8 @@ var cards = [
     'fa-bicycle',
     'fa-bomb'
 ];
+
+// global variables
 var deck = document.querySelector('.deck');
 var moveCounterSpan = document.querySelector('.moves');
 var moveCount = 0;
@@ -52,96 +54,100 @@ function shuffle(array) {
     return array;
 }
 
-
+/** 
+ * capture click event on any card in deck
+*/
 deck.addEventListener('click', function(e) {
+    if (openCards.length < 2) {
+        const selection = e.target;
 
-    const selection = e.target;
+        if (selection.classList.contains('card')) {
+            showCard(selection);
+            addCard(selection);
 
-    if (selection.classList.contains('card')) {
-        showCard(selection);
-        // check if cards match
-        if (openCards.length > 1) {
-            moveCounter();
-            if (openCards[0].value === openCards[1].value) {
-                isMatch();
-                if (pairCount === 1) {
-                    completed();
-                    restartGame();
+            // check if cards match
+            if (openCards.length > 1) {
+                moveCounter();
+                if (openCards[0].value === openCards[1].value) {
+                    isMatch();
+                    if (pairCount === 8) {
+                        completed();
+                        restartGame();
+                    }
+                } else {
+                    noMatch();
                 }
-            } else {
-                noMatch();
             }
         }
-    }
-
-    // show selected card
-    function showCard(arg) {
-        arg.classList.add('open', 'show');
-        addCard(arg);
-    };
-
-    // add card to openCards
-    function addCard(arg) {
-        let icon = arg.firstElementChild.classList;
-        return openCards.push(icon);
-    };
-
-    // cards match
-    function isMatch() {
-        for (const card of deck.children) {
-            if (card.classList.contains('open')) {
-                card.classList.add('match');
-                card.classList.remove('open', 'show');
-            }
-        }
-        openCards = [];
-        pairCount += 1;
-    }
-
-    // cards dont match
-    function noMatch() {
-        setTimeout(function () {
-            for (const card of deck.children) {
-                card.classList.remove('open', 'show');
-            }
-            openCards = [];
-        }, 1000);
-    }
-
-    // count player's moves
-    function moveCounter() {
-        moveCount += 1;
-        moveCounterSpan.innerHTML = moveCount;
-    }
-
-    // game is completed
-    function completed() {
-        setTimeout(function () {
-            var successContainer = document.querySelector('.success-container');
-            var successCount = document.querySelector('.success-count');
-            successCount.innerHTML = moveCount;
-            successContainer.style.display = 'flex';
-        },300);
-    }
-
-    // restart game
-    function restartGame() {
-        var restartButton = document.querySelector('.restart-button');
-        restartButton.addEventListener('click', function (){
-            var eachCard = deck.querySelectorAll('.card'); /** this is the li array */
-            var successContainer = document.querySelector('.success-container');
-
-            successContainer.style.display = 'none';
-            moveCounterSpan.innerHTML = 0;
-            shuffle(cards);
-
-            for (i = 0; i < eachCard.length; i++) {
-                eachCard[i].classList.remove('match');
-                eachCard[i].firstElementChild.classList = 'fa ' + cards[i];
-            }
-            moveCount = 0;
-            pairCount = 0;
-        });
     }
 
 });
+
+// show selected card
+function showCard(param) {
+    param.classList.add('open', 'show');
+};
+
+// add card to openCards
+function addCard(param) {
+    var icon = param.firstElementChild.classList;
+    return openCards.push(icon);
+};
+
+// cards match
+function isMatch() {
+    for (const card of deck.children) {
+        if (card.classList.contains('open')) {
+            card.classList.add('match');
+            card.classList.remove('open', 'show');
+        }
+    }
+    openCards = [];
+    pairCount += 1;
+}
+
+// cards dont match
+function noMatch() {
+    setTimeout(function () {
+        for (const card of deck.children) {
+            card.classList.remove('open', 'show');
+        }
+        openCards = [];
+    }, 1000);
+}
+
+// count player's moves
+function moveCounter() {
+    moveCount += 1;
+    moveCounterSpan.innerHTML = moveCount;
+}
+
+// game is completed
+function completed() {
+    setTimeout(function () {
+        var successContainer = document.querySelector('.success-container');
+        var successCount = document.querySelector('.success-count');
+        successCount.innerHTML = moveCount;
+        successContainer.style.display = 'flex';
+    },300);
+}
+
+// restart game
+function restartGame() {
+    var restartButton = document.querySelector('.restart-button');
+    restartButton.addEventListener('click', function (){
+        var eachCard = deck.querySelectorAll('.card');
+        var successContainer = document.querySelector('.success-container');
+
+        successContainer.style.display = 'none';
+        moveCounterSpan.innerHTML = 0;
+        shuffle(cards);
+
+        for (i = 0; i < eachCard.length; i++) {
+            eachCard[i].classList.remove('match');
+            eachCard[i].firstElementChild.classList = 'fa ' + cards[i];
+        }
+        moveCount = 0;
+        pairCount = 0;
+    });
+}
